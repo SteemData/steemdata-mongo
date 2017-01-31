d28fe05f9dc4fd8544398c697f6fb1f684069671
furion <_@furion.me>
2017-02-01 00:58:58 +0100

    minor refactoring

--- src/helpers.py
+++ src/helpers.py
@@ -2,7 +2,48 @@ import time
 from contextlib import contextmanager
 from datetime import datetime
 
+from funcy import flatten
+from steem import Steem
 from steemdata.markets import Markets
+
+
+def fetch_comments_flat(root_post=None, comments=list(), all_comments=list()):
+    """
+    Recursively fetch all the child comments, and return them as a list.
+
+    Usage: all_comments = fetch_comments_flat(Post('@foo/bar'))
+    """
+    # see if our root post has any comments
+    if root_post:
+        return fetch_comments_flat(comments=root_post.get_comments())
+    if not comments:
+        return all_comments
+
+    # recursively scrape children one depth layer at a time
+    children = list(flatten([x.get_comments() for x in comments]))
+    if not children:
+        return all_comments
+    return fetch_comments_flat(all_comments=comments + children, comments=children)
+
+
+def get_all_usernames(last_user=-1, steem=None):
+    if not steem:
+        steem = Steem()
+
+    usernames = steem.rpc.lookup_accounts(last_user, 1000)
+    batch = []
+    while len(batch) != 1:
+        batch = steem.rpc.lookup_accounts(usernames[-1], 1000)
+        usernames += batch[1:]
+
+    return usernames
+
+
+def get_usernames_batch(last_user=-1, steem=None):
+    if not steem:
+        steem = Steem()
+
+    return steem.rpc.lookup_accounts(last_user, 1000)
 
 
 @contextmanager
--- src/scraper.py
+++ src/scraper.py
@@ -2,16 +2,14 @@ import time
 from contextlib import suppress
 
 import pymongo
-from funcy import flatten
 from pymongo.errors import DuplicateKeyError
-from steem import Steem
 from steem.account import Account
 from steem.exceptions import PostDoesNotExist
 from steem.post import Post
 from steem.utils import is_comment
 from steemdata.blockchain import Blockchain, typify
 
-from helpers import fetch_price_feed
+from helpers import fetch_price_feed, get_usernames_batch
 from mongostorage import MongoStorage, Settings, Stats
 
 
@@ -144,45 +142,6 @@ def update_account_ops(mongo, steem, username):
             # parse fields
             event = typify(event)
             mongo.AccountOperations.insert_one(event)
-
-
-def _fetch_comments_flat(root_post=None, comments=list(), all_comments=list()):
-    """
-    Recursively fetch all the child comments, and return them as a list.
-
-    Usage: all_comments = fetch_comments_flat(Post('@foo/bar'))
-    """
-    # see if our root post has any comments
-    if root_post:
-        return _fetch_comments_flat(comments=root_post.get_comments())
-    if not comments:
-        return all_comments
-
-    # recursively scrape children one depth layer at a time
-    children = list(flatten([x.get_comments() for x in comments]))
-    if not children:
-        return all_comments
-    return _fetch_comments_flat(all_comments=comments + children, comments=children)
-
-
-def get_all_usernames(last_user=-1, steem=None):
-    if not steem:
-        steem = Steem()
-
-    usernames = steem.rpc.lookup_accounts(last_user, 1000)
-    batch = []
-    while len(batch) != 1:
-        batch = steem.rpc.lookup_accounts(usernames[-1], 1000)
-        usernames += batch[1:]
-
-    return usernames
-
-
-def get_usernames_batch(last_user=-1, steem=None):
-    if not steem:
-        steem = Steem()
-
-    return steem.rpc.lookup_accounts(last_user, 1000)
 
 
 def override(mongo):
