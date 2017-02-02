7d97e86f5cecc3cde4a53b9c973a5483106d835f
furion <_@furion.me>
2017-02-02 18:30:47 +0100

    [runner] dedup run_worker

--- src/__main__.py
+++ src/__main__.py
@@ -1,48 +1,7 @@
 import getopt
-import os
 import sys
-import time
-import traceback
 
-from steem import Steem
-
-from mongostorage import MongoStorage
-from scraper import (
-    scrape_all_users,
-    scrape_operations,
-    scrape_active_posts,
-    scrape_misc,
-    override
-)
-
-
-def run_worker(worker_name):
-    mongo = MongoStorage(db_name=os.environ['DB_NAME'],
-                         host=os.environ['DB_HOST'],
-                         port=os.environ['DB_PORT'])
-    mongo.ensure_indexes()
-    stm = Steem()
-
-    while True:
-        try:
-            if worker_name == "scrape_all_users":
-                scrape_all_users(mongo, stm)
-            elif worker_name == "scrape_operations":
-                scrape_operations(mongo, stm)
-            elif worker_name == "scrape_active_posts":
-                scrape_active_posts(mongo, stm)
-            elif worker_name == "scrape_misc":
-                scrape_misc(mongo)
-            elif worker_name == "override":
-                override(mongo)
-        except (KeyboardInterrupt, SystemExit):
-            exit("Quitting...")
-        except:
-            print("EXCEPTION: %s():" % worker_name)
-            print(traceback.format_exc())
-
-        # prevent IO overflow
-        time.sleep(5)
+from runner import run_worker
 
 
 def start_worker(argv):
--- src/runner.py
+++ src/runner.py
@@ -1,21 +1,24 @@
 import multiprocessing
+import os
 import time
 import traceback
 from multiprocessing.pool import Pool
 
 from steem import Steem
 
-from mongostorage import MongoStorage
+from mongostorage import MongoStorage, MONGO_HOST, DB_NAME, MONGO_PORT
 from scraper import (
     scrape_all_users,
     scrape_operations,
     scrape_active_posts,
-    scrape_misc
-)
+    scrape_misc,
+    override)
 
 
 def run_worker(worker_name):
-    mongo = MongoStorage()
+    mongo = MongoStorage(db_name=os.getenv('DB_NAME', DB_NAME),
+                         host=os.getenv('DB_HOST', MONGO_HOST),
+                         port=os.getenv('DB_PORT', MONGO_PORT))
     mongo.ensure_indexes()
     stm = Steem()
 
@@ -29,6 +32,8 @@ def run_worker(worker_name):
                 scrape_active_posts(mongo, stm)
             elif worker_name == "scrape_misc":
                 scrape_misc(mongo)
+            elif worker_name == "override":
+                override(mongo)
         except (KeyboardInterrupt, SystemExit):
             exit("Quitting...")
         except:
